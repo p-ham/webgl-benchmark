e7a27142805897ba20c31fadaeffcf80adf4b2d7
Philipp Hamerle <phamerle.mmt-b2016@fh-salzburg.ac.at>
2019-05-04 20:39:32 +0200

    First unity build with 2000 rotating cubes

---
+++ New Unity Project/Assets/CubeBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right, speed * Time.deltaTime * 1);
+        transform.Rotate(Vector3.up, speed * Time.deltaTime * 3);
+    }
+}
---
+++ New Unity Project/Assets/MainBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBehaviour : MonoBehaviour
+{
+    public GameObject cube;
+
+    private List<GameObject> cubes = new List<GameObject>();
+    private int cubesCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cubesCount < 1000) {
+            // create a copy of the cube prefab
+            cubes.Add(Instantiate(cube, getRandomPosition(), Random.rotation));
+            // change the color of the copy
+            cubes[cubesCount].GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            cubesCount++;
+        }
+
+    }
+
+    Vector3 getRandomPosition() {
+        int x = Random.Range(-5, 5);
+        int y = Random.Range(-5, 5);
+        int z = Random.Range(-5, 5);
+        return new Vector3(x, y, z);
+    }
+}
